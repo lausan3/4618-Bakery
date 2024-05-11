177062ae58e3c94d79981ad34943c68e93911960
lausan3 <lauanthony4455@gmail.com>
2024-05-11 12:50:27 -0400

    Added layouts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
+import Layout from './routes/Layout.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route index={true} element={<App/>} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
