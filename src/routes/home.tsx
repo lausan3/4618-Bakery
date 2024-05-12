ea497e5dbed97a2e4c2846ef74c5066a1d9ed18e
lausan3 <lauanthony4455@gmail.com>
2024-05-11 20:13:02 -0400

    Push Home Hero resizing and grid changes

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,19 @@
 const Home = () => {
   return (
-    <div id="home-container" className="flex w-full h-full bg-gray-600 justify-center">
-      <div id="hero" className="flex flex-row w-full h-1/2 px-20 center-size">
-        <div id="show-stack" className="flex flex-col w-40 justify-center">
-          <span className="text-center self-center text-4xl font-semibold">Order cakes from us!</span>
-          <span className="text-center self-center">We provide classic cakes and make it anyway you want!</span>
+    <div id="home-container" className="
+      flex w-full h-full bg-gray-300 justify-center py-10 
+      max-sm:py-10"
+    >
+      <div id="hero" className="
+        grid grid-cols-2 h-1/2 px-10 center-size
+        max-sm:grid-rows-2 max-sm:grid-cols-1"
+      >
+        
+        <div id="show-stack" className="flex flex-col justify-center">
+          <span className="text-center self-center text-3xl font-semibold">Custom-Made Cakes for any Occasion.</span>
+          <span className="text-center self-center text-xl mt-3">We provide cakes made-to-order for your event</span>
         </div>
-        <img src="./IMG_0003.PNG" alt="Showcase Cake" className="w-1/5 object-contain"/>
+        <img src="./IMG_0003.PNG" alt="Showcase Cake" className="w-3/4 px-5 py-5 mx-auto object-contain"/>
       </div>
     </div>
   )
