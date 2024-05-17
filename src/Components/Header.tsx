c287e287d777d5a1aeaac1f6a2551f2df1a051cb
lausan3 <lauanthony4455@gmail.com>
2024-05-17 17:36:14 -0400

    Refactor to mobile layout, need to fix custom made cakes text centering.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,20 +2,19 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-center w-full py-10 bg-main-color
+    <header className="flex flex-row justify-center w-full py-8 bg-main-color
     shadow-2xl z-[2]
     ">
       <div className="
-        flex flex-row items-center justify-between center-size
-        max-sm:flex-col"
+        flex flex-col items-center justify-between center-size
+        sm:flex-row"
       >
         <Link to="/" className="
           text-2xl"
         >Meet Cake Bakery</Link>
 
         <div className="
-          flex flex-row ml-auto px-5 gap-2
-          max-sm:mt-4"
+          flex flex-row ml-auto px-5 my-2 gap-2"
         >
           <Link to="/location" className="text-xl link-underline">Location</Link>
           <Link to="/showcase" className="text-xl link-underline">Showcase</Link>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,23 +1,23 @@
 const Home = () => {
   return (
     <div id="home-container" className="
-      flex w-full h-full bg-gray-300 justify-center py-10 
-      max-sm:py-10"
+      flex w-full h-full bg-gray-300 justify-center py-16
+      sm:py-5"
     >
       <div id="hero" className="
-        grid grid-cols-2 h-1/2 px-10 center-size
-        max-sm:grid-rows-2 max-sm:grid-cols-1"
+        grid grid-rows-2 h-1/2 px-10 center-size
+        sm:grid-cols-2 sm:grid-rows-1"
       >
         
         <div id="show-stack" className="
-          flex flex-col justify-center"
+          flex flex-col"
         >
           <span className="text-center self-center text-3xl font-semibold">Custom-Made Cakes for any Occasion.</span>
           <span className="text-center self-center text-xl mt-3">We provide cakes made-to-order for your event</span>
         </div>
         <img src="./IMG_0003.PNG" alt="Showcase Cake" className="
-          w-3/4 px-5 py-5 mx-auto object-contain
-          max-sm:mt-4"
+          w-3/4 px-5 py-5 mt-6 mx-auto object-contain
+          sm:mt-0"
         />
       </div>
     </div>
