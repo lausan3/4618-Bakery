587c21be99943535e848a3dd1af43261af0ac962
lausan3 <lauanthony4455@gmail.com>
2024-05-24 12:33:48 -0400

    Add scrapped idea for support link

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,9 +1,14 @@
+// import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
     <footer className="
-      flex flex-row justify-center w-full py-8 bg-main-color
+      flex flex-col justify-center items-center w-full py-8 bg-main-color gap-y-2
+      sm:flex-row
     ">
-      <a href="https://github.com/lausan3" className="text-base sm:text-xl">Designed by Anthony Lau</a>
+      <a href="https://github.com/lausan3" target="_blank" className="text-base sm:text-xl">Designed by Anthony Lau</a>
+      {/* <div id="spacer" className="w-20"></div> */}
+      {/* <Link className="text-base sm:text-lg" to="/support">Having trouble with our website?</Link> */}
     </footer>
   )
 }
