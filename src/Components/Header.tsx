3f0eb3ae8196dd89b04ea554e5d2fad525b4f6be
lausan3 <lauanthony4455@gmail.com>
2024-05-24 12:46:23 -0400

    Added spacing to header items

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <header className="
-      flex flex-row justify-center w-full py-8 bg-main-color
+      flex flex-row justify-center w-full px-10 py-8 bg-main-color
       shadow-2xl z-[2]
     ">
       <div className="
-        flex flex-col items-center justify-between center-size
-        sm:flex-row"
+        flex flex-col items-center gap-y-1
+        sm:flex-row sm:center-size"
       >
         <Link to="/" className="
-          text-2xl"
+          text-2xl text-nowrap"
         >Meet Cake Bakery</Link>
 
+        <div className="w-52"></div>
+
         <div className="
-          flex flex-row ml-auto px-5 my-2 gap-2"
+          flex flex-row px-5 my-2 gap-5"
         >
           <Link to="/location" className="text-xl link-underline">Location</Link>
           <Link to="/showcase" className="text-xl link-underline">Showcase</Link>
