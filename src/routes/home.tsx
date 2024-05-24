b50fd0dc02ca1a0831171d821049da9e81f28f3e
lausan3 <lauanthony4455@gmail.com>
2024-05-24 16:57:42 -0400

    Made Carousel size consistent

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,6 @@
 import { Carousel } from "@mantine/carousel"
+import { Image } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { Link } from "react-router-dom"
 
 const Home = () => {
@@ -18,8 +20,23 @@ const Home = () => {
     './_com.apple.Pasteboard.Pa3lOk.PNG',
     './_com.apple.Pasteboard.S9ZbU9.PNG',
     './_com.apple.Pasteboard.tfHHHi.PNG',
-    './IMG_1155.png',
+    './IMG_1155.PNG',
   ];
+
+  const isMobile = useMediaQuery(`(max-width: 640px)`);
+
+  const slides = images.map( (url) => {
+      return (
+        <Carousel.Slide key={url}>
+          <Image 
+            src={url}
+            style={{ height: isMobile ? '110px' : '500px', objectFit: 'contain'}}
+          />
+        </Carousel.Slide>
+      )
+    }
+  )
+
 
   return (
     <div id="home-container" className="
@@ -28,16 +45,14 @@ const Home = () => {
     ">
       <div id="hero" className="
         grid grid-rows-[auto,1fr] h-[60%] px-10 center-size
-        sm:grid-cols-2 sm:grid-rows-1 sm:h-[690px] sm:items-center sm:justify-items-center
+        sm:grid-cols-2 sm:grid-rows-1 sm:h-[680px] sm:items-center sm:justify-items-center
       ">
         <div id="hero-stack" className="
           flex flex-col text-center items-center gap-5
         ">
           <span className="text-3xl font-semibold">Custom-Made Cakes for any Occasion.</span>
           <span className="text-xl">We provide cakes made-to-order for your event</span>
-          <Link className="
-            px-4 py-2 bg-main-color rounded-3xl
-          " to="/order">Contact Us</Link>
+          <Link className="px-4 py-2 bg-main-color rounded-lg font-semibold" to="/order">Contact Us</Link>
         </div>
         <img src="./IMG_0003.PNG" alt="Showcase Cake" className="
           p-10
@@ -46,36 +61,26 @@ const Home = () => {
       </div>
 
       <div id="carousel" className="
-        flex flex-col px-10 pt-8 pb-20 mt-10 bg-gray-300
+        flex flex-col px-10 py-8 mt-10 bg-gray-300
         shadow-2xl z-[2]
-        sm:items-center sm:center-size sm:min-w-full
+        sm:center-size sm:min-w-full sm:h-fit
       ">
         <span className="
-          px-5 py-5
-          text-xl text-center font-semibold 
+          px-5 py-5 self-center
+          text-xl text-center font-semibold
           sm:w-[40%]
         ">We've served the community for over 20 years making quality, classic cakes for parties, weddings, and more!</span>
         <Carousel 
           className="mt-5 sm:mt-12"
           slideSize="30%" 
           slideGap="xl"
-          slidesToScroll={3}
+          slidesToScroll={2}
           align="start"
           withIndicators
           dragFree
-          includeGapInSize={false}
-          controlSize={50}
+          controlSize={isMobile ? 26 : 50}
         >
-          {
-            images.map( (url) => {
-                return (
-                  <Carousel.Slide key={url}>
-                    <img src={url}/>
-                  </Carousel.Slide>
-                )
-              }
-            )
-          }
+          {slides}
         </Carousel>
       </div>
     </div>
