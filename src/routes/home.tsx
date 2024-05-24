6145f281eee4e1f75138294fde57bc4cab7e556b
lausan3 <lauanthony4455@gmail.com>
2024-05-24 15:35:27 -0400

    Moved assets to public for deployment

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   const images = [
-    './src/assets/IMG_6042.PNG',
-    './src/assets/IMG_6043.PNG',
-    './src/assets/IMG_6044.PNG',
-    './src/assets/IMG_6045.PNG',
-    './src/assets/IMG_6046.PNG',
-    './src/assets/IMG_6050.PNG',
-    './src/assets/IMG_6051.PNG',
-    './src/assets/IMG_6052.PNG',
-    './src/assets/IMG_6053.PNG',
-    './src/assets/IMG_6054.PNG',
-    './src/assets/IMG_6055.PNG',
-    './src/assets/_com.apple.Pasteboard.Aydaf0.PNG',
-    './src/assets/_com.apple.Pasteboard.Pa3lOk.PNG',
-    './src/assets/_com.apple.Pasteboard.S9ZbU9.PNG',
-    './src/assets/_com.apple.Pasteboard.tfHHHi.PNG',
-    './src/assets/IMG_1155.png',
+    './IMG_6042.PNG',
+    './IMG_6043.PNG',
+    './IMG_6044.PNG',
+    './IMG_6045.PNG',
+    './IMG_6046.PNG',
+    './IMG_6050.PNG',
+    './IMG_6051.PNG',
+    './IMG_6052.PNG',
+    './IMG_6053.PNG',
+    './IMG_6054.PNG',
+    './IMG_6055.PNG',
+    './_com.apple.Pasteboard.Aydaf0.PNG',
+    './_com.apple.Pasteboard.Pa3lOk.PNG',
+    './_com.apple.Pasteboard.S9ZbU9.PNG',
+    './_com.apple.Pasteboard.tfHHHi.PNG',
+    './IMG_1155.png',
   ];
 
   return (
@@ -39,7 +39,7 @@ const Home = () => {
             px-4 py-2 bg-main-color rounded-3xl
           " to="/order">Contact Us</Link>
         </div>
-        <img src="./src/assets/IMG_0003.PNG" alt="Showcase Cake" className="
+        <img src="./IMG_0003.PNG" alt="Showcase Cake" className="
           p-10
           sm:w-[60%] sm:object-contain sm:p-0"
         />
@@ -56,7 +56,7 @@ const Home = () => {
           sm:w-[40%]
         ">We've served the community for over 20 years making quality, classic cakes for parties, weddings, and more!</span>
         <Carousel 
-          className="mt-5 sm:mt-32"
+          className="mt-5 sm:mt-12"
           slideSize="30%" 
           slideGap="xl"
           slidesToScroll={3}
