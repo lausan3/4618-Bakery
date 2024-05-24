0c0dabb6a9f50fd9917b4bcd807c8644e42aae44
lausan3 <lauanthony4455@gmail.com>
2024-05-24 11:40:43 -0400

    Finish hero

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="
       flex flex-row justify-center w-full py-8 bg-main-color
     ">
-      <a href="https://github.com/lausan3" className="text-xl">Made by Anthony Lau</a>
+      <a href="https://github.com/lausan3" className="text-base sm:text-xl">Designed by Anthony Lau</a>
     </footer>
   )
 }
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,23 +1,28 @@
 const Home = () => {
   return (
     <div id="home-container" className="
-      flex w-full h-full bg-gray-300 justify-center py-16
-      sm:py-5"
+      flex w-full h-full bg-gray-300 justify-center pt-10"
     >
       <div id="hero" className="
-        grid grid-rows-2 h-1/2 px-10 center-size
-        sm:grid-cols-2 sm:grid-rows-1"
+        grid grid-rows-[auto,1fr] px-10 center-size
+        sm:grid-cols-2 sm:grid-rows-1 sm:h-[75%] sm:items-center sm:justify-items-center"
       >
-        <div id="show-stack" className="
+        <div id="hero-stack" className="
           flex flex-col"
         >
           <span className="text-center self-center text-3xl font-semibold">Custom-Made Cakes for any Occasion.</span>
           <span className="text-center self-center text-xl mt-3">We provide cakes made-to-order for your event</span>
         </div>
         <img src="./IMG_0003.PNG" alt="Showcase Cake" className="
-          w-3/4 px-5 py-5 mt-6 mx-auto object-contain
-          sm:mt-0"
+          p-10
+          sm:w-[75%] sm:object-contain sm:p-0"
         />
+      </div>
+
+      <div id="card-showcase" className="flex">
+        <section>
+          
+        </section>
       </div>
     </div>
   )
