ab10fc20009315c00335f88231ee8666381a5d61
lausan3 <lauanthony4455@gmail.com>
2024-06-26 21:33:20 -0400

    Refactored Location css

--- src/routes/Location.tsx
+++ src/routes/Location.tsx
@@ -4,11 +4,8 @@ const Location = () => {
   return (
     <div className="main-container">
       <div className="page-container">
-        <div id="g-map-text-container" className="flex flex-col sm:flex-row">
-          <div 
-            id="g-map-container" 
-            className="w-full h-[500px]"
-          >
+        <div className="g-map-text-container">
+          <div className="g-map-container">
 
             <iframe 
               className="w-full h-full"
@@ -18,27 +15,19 @@ const Location = () => {
             />
           </div>
 
-          <div id="visit-us-text-stack" className="flex flex-col items-center text-center gap-y-8 p-8 pb-0 sm:p-8">
+          <div className="visit-us-text-stack">
 
-            <div className="flex flex-col">
+            <div className="english-chinese-stack text-3xl">
 
-              <span
-                className="text-3xl"
-              >Come visit us at 4618 8th Avenue in Brooklyn, New York!</span>
-              <span
-                className="text-3xl"
-              >歡迎來到紐約布魯克林第八大道 4618 號拜訪我們！</span>
+              <span>Come visit us at 4618 8th Avenue in Brooklyn, New York!</span>
+              <span>歡迎來到紐約布魯克林第八大道 4618 號拜訪我們！</span>
               
             </div>
 
-            <div className="flex flex-col">
+            <div className="english-chinese-stack text-2xl">
 
-              <span
-                className="text-2xl"
-              >We have a wide variety of pre-made cakes, Chinese buns, and drinks.</span>
-              <span
-                className="text-2xl"
-              >我哋有各種各樣嘅預製蛋糕、中式包子同嘢飲。</span>
+              <span>We have a wide variety of pre-made cakes, Chinese buns, and drinks.</span>
+              <span>我哋有各種各樣嘅預製蛋糕、中式包子同嘢飲。</span>
             </div>
           </div>
         </div>
